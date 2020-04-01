8f8c30ff8cd8459cc8be7e88c2cd714a130419db
tylergreason <tyler.greason@protonmail.com>
2020-04-01 15:19:49 -0400

    generated user schema, model, controller, custom login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # custom routes 
+  post '/signup', to: 'users#create'
+  # post '/login' to: 'sessions#create'
+  # post '/logout' to: 'sessions#destroy' 
+
 end
---
+++ db/migrate/20200401190733_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :city
+      t.string :state
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
