eb6986945a89b0b4026145e0862a1ea07aa651e4
tylergreason <tyler.greason@protonmail.com>
2020-04-01 15:24:50 -0400

    bcrypt working. Moving to JWT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     def create 
-        
+        User.create(user_params)
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :city, :state, :country)
     end
 end
