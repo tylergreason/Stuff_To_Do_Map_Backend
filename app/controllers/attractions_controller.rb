d3fda13e643c1b43ecb4aea67c8a5054af46e54f
tylergreason <tyler.greason@protonmail.com>
2020-04-03 10:21:38 -0400

    attractions in bounds are successfuly being returned to front end

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,12 +2,17 @@ class AttractionsController < ApplicationController
     def index 
         # byebug
         # bounds = request.headers['bounds']
-        north = request.headers['north']
-        south = request.headers['south']
-        east = request.headers['east']
-        west = request.headers['east']
+        north = request.headers['north'].to_f
+        east = request.headers['east'].to_f
+        south = request.headers['south'].to_f
+        west = request.headers['west'].to_f
 
-        attractions = Attraction.all 
+        attractions = Attraction.attractions_in_bounds(north,east,south,west)
+        p north 
+        p south
+        p west 
+        p east
+        p attractions
         render :json => attractions
     end
 
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,3 +1,9 @@
 class Attraction < ApplicationRecord
     belongs_to :user
+
+    def self.attractions_in_bounds(north, east, south, west)
+        Attraction.select {|attraction|
+            attraction.lat.between?(south,north) and attraction.lng.between?(west,east)
+        }
+    end
 end
