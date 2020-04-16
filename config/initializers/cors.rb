6de5c8b405db5c6e30fb4e7cccdef1a7e83ec280
tylergreason <tyler.greason@protonmail.com>
2020-04-16 14:49:04 -0400

    Config cors for intended frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tylergreason.github.io/Stuff_To_Do_Map_Frontend/'
+    origins 'https://tylergreason.github.io/Stuff_To_Do_Map_Frontend'
 
     resource '*',
       headers: :any,
