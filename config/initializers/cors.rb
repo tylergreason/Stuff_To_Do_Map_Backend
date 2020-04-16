014a0c73908518c17dd9f0122e804f09432f97da
tylergreason <tyler.greason@protonmail.com>
2020-04-16 15:00:18 -0400

    Reverted back to allow all access. On time crunch, will fix later.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://tylergreason.github.io/Stuff_To_Do_Map_Frontend']
+    # origins ['https://tylergreason.github.io/Stuff_To_Do_Map_Frontend']
+    origins '*'
 
     resource '*',
       headers: :any,
